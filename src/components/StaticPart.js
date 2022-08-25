a486ff212f2c3c69ae9c36bf2e4568ca55ac04d8
Anubhav Kumar <anubhav11697@gmail.com>
2022-08-25 12:13:08 +0530

    Static Part Done

--- src/components/StaticPart.js
+++ src/components/StaticPart.js
@@ -9,11 +9,16 @@ function StaticPart() {
           </Header>
 
           <ImagePart>
-
+            <img src="/images/Group.png"/>
           </ImagePart>
 
           <TextPart>
-
+            <div className="heading">
+              Online Experiences
+            </div>
+            <div className="content">
+              <p>Join unique interactive activities led by one-of-a kind hosts--all without leaving home.</p>
+            </div>
           </TextPart>
         </Container>
     )
@@ -43,9 +48,37 @@ const Header=styled.div`
 `
 const ImagePart=styled.div`
   background-color: white;
-  height: 240px;
+  height: 280px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    max-width: 67%;
+  }
 `
 const TextPart=styled.div`
-  background-color: green;
+  background-color: white;
   flex:1;
+  display: flex;
+  flex-direction: column;
+
+  .heading {
+    flex: 0.5;
+    font-family: Poppins;
+    font-weight: 600;
+    font-size: 36px;
+    padding-left: 25px;
+  }
+
+  .content {
+    flex:1;
+    padding-left: 25px;
+     p {
+       width: 300px;
+       font-size: 15px;
+       font-family: Poppins;
+       font-weight: 300;
+     }
+  }
 `
