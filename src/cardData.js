7801b76c68d806e9dfac610e52c58e7688b3bfd1
Anubhav Kumar <anubhav11697@gmail.com>
2022-08-26 14:27:08 +0530

    Dyamic part made data-driven

---
+++ src/cardData.js
@@ -0,0 +1,94 @@
+
+export const data= [
+  {
+    id:"1",
+    title:"Life lessons with Katie Zaferes",
+    description:"Get your life on track with cognitive discussion with olympian Katie Zaferes.",
+    price:"136",
+    coverImg:"katie.png",
+    stats: {
+      rating: 5.0,
+      reviewCount: 6
+    },
+    country:"USA",
+    location: "Online",
+    openSpots:27,
+    buyStatus:"SOLD OUT"
+  },
+
+  {
+    id:"2",
+    title:"Learn Wedding Photography",
+    description:"Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learnig techniques requied to leave the happy couple with memories that'll last a lifetime.",
+    price:"125",
+    coverImg:"wedding.webp",
+    stats: {
+      rating: 5.0,
+      reviewCount: 30
+    },
+    country:"Mexico",
+    location: "Online",
+    openSpots:27,
+    buyStatus:"ONLINE"
+  },
+  {
+    id:"3",
+    title:"Group Mountain Biking",
+    description:"Experience the beautiful Norwegian landscape and meet new friends all while concquering rugged terrain on your mountain bike. (Bike provided!)",
+    price:"50",
+    coverImg:"mountain-biking.jpg",
+    stats: {
+      rating: 4.8,
+      reviewCount: 2
+    },
+    country:"Canada",
+    location: "Online",
+    openSpots:27,
+    buyStatus:"SOLD OUT"
+  },
+  {
+    id:"2",
+    title:"Learn Wedding Photography",
+    description:"Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learnig techniques requied to leave the happy couple with memories that'll last a lifetime.",
+    price:"125",
+    coverImg:"wedding.webp",
+    stats: {
+      rating: 5.0,
+      reviewCount: 30
+    },
+    country:"Mexico",
+    location: "Online",
+    openSpots:27,
+    buyStatus:"ONLINE"
+  },
+  {
+    id:"1",
+    title:"Life lessons with Katie Zaferes",
+    description:"Get your life on track with cognitive discussion with olympian Katie Zaferes.",
+    price:"136",
+    coverImg:"katie.png",
+    stats: {
+      rating: 5.0,
+      reviewCount: 6
+    },
+    country:"USA",
+    location: "Online",
+    openSpots:27,
+    buyStatus:"SOLD OUT"
+  },
+  {
+    id:"3",
+    title:"Group Mountain Biking",
+    description:"Experience the beautiful Norwegian landscape and meet new friends all while concquering rugged terrain on your mountain bike. (Bike provided!)",
+    price:"50",
+    coverImg:"mountain-biking.jpg",
+    stats: {
+      rating: 4.8,
+      reviewCount: 2
+    },
+    country:"Canada",
+    location: "Online",
+    openSpots:27,
+    buyStatus:"SOLD OUT"
+  }
+];
--- src/components/DynamicPart.js
+++ src/components/DynamicPart.js
@@ -1,39 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
 import Cards from './cards'
+import {data} from '../cardData'
 
 function DynamicPart() {
+  // console.log(data);
+  const cards = data.map( item => {
+    return (
+      <Cards
+        backgroundImg={item.coverImg}
+        starRating={item.stats.rating}
+        country={item.country}
+        price={item.price}
+        maxRating={item.stats.reviewCount}
+        description={item.title}
+        buyStatus={item.buyStatus}
+      />
+    )
+  })
     return (
         <Container>
-          <Cards
-            backgroundImg="katie.png"
-            starRating="5.0"
-            country="USA"
-            price="136"
-            maxRating="6"
-            description="Life lessons with Katie Zaferes"
-            buyStatus="SOLD OUT"
-          />
-
-          <Cards
-            backgroundImg="wedding.webp"
-            starRating="5.0"
-            country="USA"
-            price="125"
-            maxRating="30"
-            description="Learn wedding photography"
-            buyStatus="ONLINE"
-          />
-
-          <Cards
-            backgroundImg="mountain-biking.jpg"
-            starRating="4.8"
-            country="USA"
-            price="50"
-            maxRating="2"
-            description="Group Mountain Biking"
-            buyStatus="ON SALE"
-          />
+          {cards}
         </Container>
     )
 }
@@ -46,4 +33,5 @@ const Container=styled.div`
   display:flex;
   align-items: center;
   gap: 15px;
+  overflow: scroll;
 `
